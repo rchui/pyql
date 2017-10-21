b4bbd367f266a0aee9c6b37640c3cb335da4737e
Ryan Chui <ryan.w.chui@gmail.com>
2017-10-21 12:57:57 -0500

    Added check_attribute and check_validity

--- Query/querylib.py
+++ Query/querylib.py
@@ -6,11 +6,12 @@ This file defines functions for querying the database tables.
 import sys
 from Interface.helplib import query_options, print_tables, print_attributes, print_query
 
-def query(tables):
+def query(tables, attributes):
     """ Main body of the query loop.
 
     Args:
         tables: names of the tables in the database.
+        attributes: attributes of each table.
 
     Returns:
         query_statement: the query to process.
@@ -24,7 +25,7 @@ def query(tables):
         elif sub_query == 'tables':
             print_tables(tables)
         elif sub_query == 'attributes':
-            print_attributes(tables)
+            print_attributes(attributes)
         elif sub_query == 'query':
             print_query(query_statement)
         elif sub_query == 'clear':
@@ -34,3 +35,77 @@ def query(tables):
         else:
             query_statement += ' ' + sub_query
     return query_statement
+
+def check_attribute(attribute, tables, attributes):
+    """ Checks the validity of an attribute.
+
+    Args:
+        attribute: attribute to check
+        table: tables in the database
+        attributes: attributes in the database
+
+    Returns:
+        result: true if valid, else false
+    """
+    if len(attribute.split('.')) == 2: # If the attribute has an identifier
+        pair = attribute.split('.')
+        # print(pair)
+        if pair[0] not in tables.keys():
+            return False
+        elif pair[1] not in attributes[pair[0]]:
+            return False
+    else: # If the attribute does not have an identifier
+        result = False
+        for _, value in attributes.items():
+            if attribute in value:
+                result = True
+                break
+        if not result:
+            return result
+    return True
+
+def is_number(value):
+    """ Checks if a string is a number or not.
+
+    Args:
+        value: the string to check
+
+    Returns:
+        True: if a number
+        False: else
+    """
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
+
+def check_valid(selects, froms, wheres, tables, attributes):
+    """ Checks the validity of the query.
+
+    Args:
+        selects: select values
+        froms: from values
+        where: where values
+        tables: tables in the database
+        attributes: attributes of the tables in the database.
+
+    Returns:
+        Boolean: true if valid, false else
+    """
+    # Check validity of tables
+    for table in froms:
+        print(table)
+        if table not in tables.keys():
+            return False
+
+    # Check validty of attributes
+    for attribute in selects:
+        print(attribute)
+        if not check_attribute(attribute, tables, attributes):
+            return False
+
+    # TODO: wheres validity
+
+
+    return True
